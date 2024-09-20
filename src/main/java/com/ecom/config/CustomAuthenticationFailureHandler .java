3d07c087877c89b1ae1fce5769b1bbec34c96a2a
Divya <45150201+Divyavani99@users.noreply.github.com>
2024-09-20 22:27:51 +0530

    Create  CustomAuthenticationFailureHandler .java

---
+++ src/main/java/com/ecom/config/CustomAuthenticationFailureHandler .java
@@ -0,0 +1,20 @@
+package com.ecom.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        
+    	request.getSession().setAttribute("SPRING_SECURITY_LAST_EXCEPTION", exception);
+        response.sendRedirect("/signin?error=true"); 
+    }
+}
